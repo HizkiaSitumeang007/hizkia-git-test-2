a2b88dd905e75e0f94f57250a182949881dff1c7
Hizkia Situmeang <hsitumeang12@gmail.com>
2022-03-27 17:59:42 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus, rem error numquam, dolor ab consectetur quis repellendus, veniam reprehenderit aut quidem expedita! Quaerat, dignissimos harum? Iusto dolorum quidem harum provident.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
